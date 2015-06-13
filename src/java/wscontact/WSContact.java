636cfbfc7481ad747cfdf120ddea4164e71bb5db
Dyan Carra <dyancarra@gmail.com>
2015-06-13 12:15:51 -0300

    organizando metodos

--- src/java/wscontact/WSContact.java
+++ src/java/wscontact/WSContact.java
@@ -3,10 +3,12 @@ package wscontact;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
 
@@ -21,13 +23,36 @@ public class WSContact {
 
   @GET
   @Produces(MediaType.TEXT_PLAIN)
-  public String getXml() {
-    return "Dyan";
+  public String all() {
+    return null;
+  }
+  
+  @GET
+  @Path("/{id}")
+  @Produces(MediaType.TEXT_PLAIN)
+  public String find(@PathParam("id") Integer id) {
+    return null;
+  }
+  
+  @POST
+  @Path("/create")
+  @Produces(MediaType.TEXT_PLAIN)
+  public String create() {
+    return null;
+  }
+  
+  @PUT
+  @Path("/update")
+  @Produces(MediaType.TEXT_PLAIN)
+  public String update() {
+    return null;
   }
 
-  @PUT
-  @Consumes("application/xml")
-  public void putXml(String content) {
+  @DELETE
+  @Path("/{id}")
+  @Produces(MediaType.TEXT_PLAIN)
+  public String destroy() {
+    return null;
   }
   
 }
